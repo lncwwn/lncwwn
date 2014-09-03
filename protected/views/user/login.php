5960f7e4c4381455e45c36f8adb0ca4380d2ffb9
victor <lncwwn@gmail.com>
2014-09-04 00:46:49 +0800

    Refined login page

--- protected/views/user/login.php
+++ protected/views/user/login.php
@@ -6,6 +6,7 @@
     <form class="form login-form">
         <input type="text" class="input" placeholder="昵称" autofocus="autofocus"/><br>
         <input type="password" class="input" placeholder="密码"/><br>
+        <input type="checkbox" class="checkbox"/><span class="remember-me">一周内自动登录</span><br>
         <button class="btn rev-btn">登录</button>
         <button class="btn gray-btn">忘记密码？</button>
     </form>
