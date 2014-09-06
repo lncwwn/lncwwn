967ea7dbe5e25db7f69c44077d3255c63239cbd3
victor <lncwwn@gmail.com>
2014-09-07 01:11:50 +0800

    Refined style

--- protected/views/account/list.php
+++ protected/views/account/list.php
@@ -13,9 +13,25 @@
         <div class="row">
             <span class="col" data-column="index">1</span>
             <span class="col" data-column="account">lncwwn@gmail.com.com</span>
-            <span class="col" data-column="passwd"></span>
-            <span class="col" data-column="time"></span>
+            <span class="col" data-column="passwd">123456789abc</span>
+            <span class="col" data-column="time">2014/08/29</span>
         </div>
+        <div class="row"></div>
+        <div class="row"></div>
+        <div class="row"></div>
+        <div class="row"></div>
+        <div class="row"></div>
+        <div class="row"></div>
+        <div class="row"></div>
+        <div class="row"></div>
+        <div class="row"></div>
+        <div class="row"></div>
+        <div class="row"></div>
+        <div class="row"></div>
+        <div class="row"></div>
+        <div class="row"></div>
+        <div class="row"></div>
+        <div class="row"></div>
         <div class="row"></div>
         <div class="row"></div>
         <div class="row"></div>
--- themes/sky/views/layouts/main.php
+++ themes/sky/views/layouts/main.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/common.css" media="screen, projection" />
 	<title></title>
 </head>
