23056864bc4d048af5ddad6b59422452ef2f0d1c
victor <lncwwn@gmail.com>
2015-08-07 01:16:43 +0800

    refined post

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -15,6 +15,18 @@
         $('.navbar-dropdown-panel').toggle();
     }).on('click', 'div:not(".js-navbar-dropdown-area")', function(e) {
         $('.navbar-dropdown-panel').hide();
+    }).on('click', '.js-show-post-content', function() {
+        var parent = $(this).closest('.post-body');
+        parent.find('.js-post-summary').addClass('hidden');
+        parent.find('.js-post-content').removeClass('hidden');
+        parent.find('.js-hide-post-content').removeClass('hidden');
+        $(this).addClass('hidden');
+    }).on('click', '.js-hide-post-content', function() {
+        var parent = $(this).closest('.post-body');
+        parent.find('.js-post-content').addClass('hidden');
+        parent.find('.js-post-summary').removeClass('hidden');
+        parent.find('.js-show-post-content').removeClass('hidden');
+        $(this).addClass('hidden');
     });
 
 })(jQuery);
