29a0b71597b661f7e1000f773b033966793b48bc
victor <lncwwn@gmail.com>
2015-08-11 17:47:58 +0800

    refined posts page

--- public/javascripts/posts.js
+++ public/javascripts/posts.js
@@ -21,39 +21,51 @@ define([], function() {
         });
     }
 
-    $('body')
-    .off('click', '.js-show-post-content')
-    .on('click', '.js-show-post-content', function() {
-        // 显示全文
-        var parent = $(this).closest('.post-body');
-        parent.find('.js-post-summary').addClass('hidden');
-        parent.find('.js-post-content').removeClass('hidden');
-        parent.find('.js-hide-post-content').removeClass('hidden');
-        $(this).addClass('hidden');
-    })
-    .off('click', '.js-hide-post-content')
-    .on('click', '.js-hide-post-content', function() {
-        // 显示摘要
-        var postBody = $(this).closest('.post-body');
-        var postArea = $(this).closest('.js-post-interact');
-        var interactArea = postArea.prev();
+    /**
+     * 收起摘要，显示全文
+     */
+    function showContent($that) {
+        var postBody = $that.closest('.post-body');
+        var interact = $that.closest('.post-panel').find('.post-interact');
+        postBody.find('.js-post-summary').addClass('hidden');
+        postBody.find('.js-post-content').removeClass('hidden');
+        postBody.find('.js-hide-post-content').removeClass('hidden');
+        $that.addClass('hidden');
+        interact.find('.js-post-interact-info').addClass('hidden');
+        interact.find('.js-post-interact-fun').removeClass('hidden');
+    }
+
+    /**
+     * 收起全文，显示摘要
+     */
+    function showSummary($that) {
+        var postBody = $that.closest('.post-body');
+        var interact = $that.closest('.post-panel').find('.post-interact');
         postBody.find('.js-post-content').addClass('hidden');
         postBody.find('.js-post-summary').removeClass('hidden');
         postBody.find('.js-show-post-content').removeClass('hidden');
-        interactArea.find('.js-post-interact-info').hide();
-        interactArea.find('.js-post-interact').show();
-        $(this).addClass('hidden');
+        $that.addClass('hidden');
+        interact.find('.js-post-interact-fun').addClass('hidden');
+        interact.find('.js-post-interact-info').removeClass('hidden');
+    }
+
+    $('body')
+    .off('click', '.js-show-post-content')
+    .on('click', '.js-show-post-content', function() { // 显示全文
+        showContent($(this));
+    })
+    .off('click', '.js-hide-post-content')
+    .on('click', '.js-hide-post-content', function() { // 显示摘要
+        showSummary($(this));
     })
     .off('click', '.js-post-assent')
-    .on('click', '.js-post-assent', function() {
-        // 赞同文章
+    .on('click', '.js-post-assent', function() { // 赞同文章
         var userId = $('#js-current-user').data('current-user');
         var postId = $(this).closest('.js-post-interact').data('post-id');
         assentPost(this, userId, postId);
     })
     .off('click', '.js-post-oppose')
-    .on('click', '.js-post-oppose', function() {
-        // 不赞同文章
+    .on('click', '.js-post-oppose', function() { // 不赞同文章
         alert();
     })
     .off('click', '#js-edit-post-link')
